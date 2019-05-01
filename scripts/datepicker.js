32e706ac103642f4684a2153e6fe8611722430e6
Aliaksandr <a.andrasiuk@gmail.com>
2019-05-01 20:13:37 +0200

    Updates

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -1,128 +1,120 @@
-	const getDate = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
+const getDate = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
+const awaiter = document.querySelector('.awaiter');
 
-	document.querySelector('.calendar input').setAttribute('min', getDate);
-
-	const awaiter = document.querySelector('.awaiter');
+const showHours = reservedHours => {
+	let ul = document.querySelector('.date__hour');
+	let list = '<h3>Wybierz godzinę:</h3><ul class="animated fadeInUp">';
 	
-	document.querySelector('.calendar').addEventListener('change', () => {
-		document.querySelector('input[name="hour"]').removeAttribute('value');
-		document.querySelector('input[name="duration"]').removeAttribute('value');
-		let reservedHours = [12];
-		dataAwaitAnimationStart();
-		setTimeout(function(){
-			dataAwaitAnimationEnd();
-			showSessions();	
-			showHours(reservedHours);
-			addSelection(reservedHours);
-		}, 500)
-	})
-
-	const showHours = reservedHours => {
-		let ul = document.querySelector('.date__hour');
-		let list = '<h3>Wybierz godzinę:</h3><ul class="animated fadeInUp">';
+	for (let i = 8; i <= 15; i++){
+		let classes = '',
+				title = '';
 		
-		for (let i = 8; i <= 15; i++){
-			let classes = '',
-					title = '';
-			if (reservedHours.includes(i)){
-					classes += 'disabled';
-					title    = 'Ta godzina jest zajęta';
-			}
-			list += '<li class="' + classes +'" title="'+ title +'">' + i + '<span></span></li>';
+		if (reservedHours.includes(i)){
+				classes += 'disabled';
+				title    = 'Ta godzina jest zajęta';
 		}
-			list += '</ul>';
-			ul.innerHTML = list;						
+		
+		list += `<li class="${classes}" title="${title}">${i}<span></span></li>`;
 	}
 
-	const showSessions = () => { 
-		let ul = document.querySelector('.date__session');
-		let list = '<h3>Wybierz taryfę:</h3><ul class="session animated fadeInUp">';
-		let sessionsArray = ['10zł / 1 Godzina', '20zł / 2 Godziny', '0zł / 2 Godziny'];
-		for (let i = 0; i < sessionsArray.length; i++){
-			list += '<li class="session__item">' + sessionsArray[i] + '<span></span></li>';
-		}
-			list += '</ul>';
-			ul.innerHTML = list;					
+	list += '</ul>';
+	ul.innerHTML = list;						
+}
+
+const showSessions = () => { 
+	let ul = document.querySelector('.date__session');
+	let list = '<h3>Wybierz taryfę:</h3><ul class="session animated fadeInUp">';
+	const sessionsArray = ['10zł / 1 Godzina', '20zł / 2 Godziny', '0zł / 2 Godziny'];
+
+	for (let i = 0; i < sessionsArray.length; i++){
+		list += `<li class="session__item">${sessionsArray[i]}'<span></span></li>`;
 	}
 
+	list += '</ul>';
+	ul.innerHTML = list;					
+}
 
-	const addSelection = trueHours => {
-		let list = document.querySelectorAll('li[class=""]');
-		let sessions = document.querySelectorAll('li[class*="session__item"]');
+const addSelection = trueHours => {
+   const list = document.querySelectorAll('li[class=""]');
+   const sessions = document.querySelectorAll('li[class*="session__item"]');
 
-		for (li of list){
-				li.addEventListener('click', function(){
-					deleteSelection(li);	
-					this.setAttribute('selected','1');	
-					document.querySelector('input[name="hour"]').value = parseInt(this.innerHTML);
-				});
-		}
-		for (session of sessions){
-				session.addEventListener('click', function(){
-					deleteSelection(session);
-					resetHours(trueHours);
-					this.setAttribute('selected','1');
-					document.querySelector('input[name="duration"]').value = parseInt(this.innerHTML.split(' / ')[1][0]);
-					
-					currentSession = parseInt(this.innerHTML.split(' / ')[1][0]);
-					if (currentSession === 2){	
-						disableHoursForSessionLength();
-					}
-					
-					selected = document.querySelectorAll('.date__hour li[selected="1"]');
-					for (select of selected){
-						select.removeAttribute('selected');	
-					}
-					
-				});
-		}	
-	}
+   for (li of list) {
+      li.addEventListener("click", function() {
+         deleteSelection(li);
+         this.setAttribute("selected", "1");
+         hour.value = parseInt(this.innerHTML);
+      });
+   }
 
-	const deleteSelection = type => {
-		let selected;
-		if (type.getAttribute('class') !== ""){
-			selected = document.querySelectorAll('.date__session li[selected="1"]');	
-		}
-		else {
-			selected = document.querySelectorAll('.date__hour li[selected="1"]');
-		}
-		for (select of selected){
-				select.removeAttribute('selected');
-		}
-	}
-	
-	const resetHours = trueHours => {
-		let hours = document.querySelectorAll('.date__hour li');
-		hours = [...hours];
-		for (let i = 8; i <= 15; i++){
-			if (!trueHours.includes(i)){
-				hours[i - 8].classList.remove('disabled');
-			}
-		}
-	}
+   for (session of sessions) {
+      session.addEventListener("click", function() {
+         deleteSelection(session);
+         hour.removeAttribute("value");
+         resetHours(trueHours);
+         this.setAttribute("selected", "1");
 
-	const dataAwaitAnimationStart = () => {
-		awaiter.style.display = "block";
-		toAnimateDarkBackground(reservationForm, 1, 0.3, 10);
-	}
-	
+         duration.value = parseInt(this.innerHTML.split(" / ")[1][0]);
 
-	const dataAwaitAnimationEnd =() => {
-		awaiter.style.display = "none";
-		toAnimateLightBackground(reservationForm, 0.3, 1, 10);
-	}
+         currentSession = parseInt(this.innerHTML.split(" / ")[1][0]);
+         currentSession === 2 && disableHoursForSessionLength();
 
-	const disableHoursForSessionLength = () => {
-		let hours = document.querySelectorAll('.date__hour li');
-		hours = [...hours];
+         selected = document.querySelectorAll('.date__hour li[selected="1"]');
 
-		for (let i = 0; i < hours.length - 1; i++){
-			if (hours[i + 1].getAttribute('class') === 'disabled'){
-				hours[i].classList.add('disabled');
-			}
-		}
-	}
+         selected.forEach(select => select.removeAttribute("selected"));
+      });
+   }
+};
+
+const deleteSelection = type => {
+   let selected =
+      type.getAttribute("class") !== ""
+         ? document.querySelectorAll('.date__session li[selected="1"]')
+         : document.querySelectorAll('.date__hour li[selected="1"]');
+
+   selected.forEach(select => select.removeAttribute("selected"));
+};
+
+const resetHours = trueHours => {
+   let hours = [...document.querySelectorAll(".date__hour li")];
+
+   for (let i = 8; i <= 15; i++) {
+      !trueHours.includes(i) && hours[i - 8].classList.remove("disabled");
+   }
+};
+
+const dataAwaitAnimation = direction => {
+   if (direction === "start") {
+      awaiter.style.display = "block";
+      animateBackground(reservationForm, 1, 0.3, 10);
+   } else {
+      awaiter.style.display = "none";
+      animateBackground(reservationForm, 0.3, 1, 10);
+   }
+};
+
+const disableHoursForSessionLength = () => {
+   let hours = [...document.querySelectorAll(".date__hour li")];
+
+   for (let i = 0; i < hours.length - 1; i++) {
+      hours[i + 1].getAttribute("class") === "disabled" && hours[i].classList.add("disabled");
+   }
+};
+
+document.querySelector(".calendar input").setAttribute("min", getDate);
 
 
+document.querySelector(".calendar").addEventListener("change", () => {
+   hour.removeAttribute("value");
+   duration.removeAttribute("value");
 
+   let reservedHours = [12];
 
+   dataAwaitAnimation("start");
+
+   setTimeout(() => {
+      dataAwaitAnimation("end");
+      showSessions();
+      showHours(reservedHours);
+      addSelection(reservedHours);
+   }, 500);
+});
--- scripts/formValidator.js
+++ scripts/formValidator.js
@@ -1,104 +1,88 @@
-	const form 				 = document.querySelector('.userdata');
-	const submitBtn 		 = document.querySelector('.userdata button');
-	const hour 				 = document.querySelector('input[name="hour"]');
-	const duration        = document.querySelector('input[name="duration"]');
-	const validateIt 		 = document.querySelectorAll(`input[name="name"], 
-																		input[name="surname"], 
-																		input[name="email"], 
-																		input[name="phoneNumber"],
-																		input[name="toReserveDate1"],
-																		input[name="hour"]`
-																		);
+const form 				 = document.querySelector('.userdata');
+const submitBtn 		 = document.querySelector('.userdata button');
+const validateIt 		 = document.querySelectorAll(`input[name="name"], 
+																	input[name="surname"], 
+																	input[name="email"], 
+																	input[name="phoneNumber"],
+																	input[name="toReserveDate1"],
+																	input[name="hour"]`
+																	);
+
+const nameRegExp 		   = /^[a-zA-z]{3,12}$/;
+const emailRegExp       = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
+const phoneNumberRegExp = /^(?:\(?\+?48)?(?:[-\.\(\)\s]*(\d)){9}\)?$/;	
+const dateRegExp 		   = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+const validationResult  = {
+	name 				: false,
+	surname 			: false,
+	phoneNumber 	: false,
+	email 			: false,
+	toReserveDate1 : false,
+	hour				: false,
+	duration       : false
+};
+
+validationResult.isAllTrue = function() {
+	let res = true;
 	
+   for (key in this) {
+      if (this[key] === false) {
+         res = false;
+         break;
+      }
+   }
+   return res;
+};
+  
+const validateField = (field, value) => {
+	if (field === "email") { return emailRegExp.test(value); }
+
+	if (field === "name" || field === "surname") { return nameRegExp.test(value); }
 	
-	const nameRegExp 		   = /^[a-zA-z]{3,12}$/;
-	const emailRegExp       = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
-	const phoneNumberRegExp = /^(?:\(?\+?48)?(?:[-\.\(\)\s]*(\d)){9}\)?$/;	
-	const dateRegExp 		   = /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
-	const validationResult  = {
-		name 				: false,
-		surname 			: false,
-		phoneNumber 	: false,
-		email 			: false,
-		toReserveDate1 : false,
-		hour				: false,
-		duration       : false
-	};
-	validationResult.isAllTrue = function(){
-		let res = true;
-		for (key in this){
-			if(this[key] === false){		
-				res = false;
-				break;
-			}
-		}
-		return res;
+	if (field === "phoneNumber") { return phoneNumberRegExp.test(value); }
+	
+   if (field === "toReserveDate1") { return dateRegExp.test(value); }
+};
+
+const setFieldValidationResult = field => {
+	validationResult[field.getAttribute('name')] = field.getAttribute('value') ? true : false;
+	allowSubmit();
+} 
+
+const hourObserver = new MutationObserver(mutations => {
+	mutations.forEach(() => setFieldValidationResult(hour));
+});
+
+const durationObserver = new MutationObserver(mutations => {
+	mutations.forEach(() => setFieldValidationResult(duration));
+});
+
+const changeColor = (obj, res) => {
+	obj.style.color = res ? "rgba(255, 255, 255, 1)" : "rgba(255, 255, 255, .5)" 
+	validationResult[obj.getAttribute('name')] = res ? true : false;
+}
+
+const allowSubmit = () => {
+	if (validationResult.isAllTrue()) {
+		submitBtn.removeAttribute('disabled');
+		submitBtn.style.color = "rgba(255, 255, 255, 1)";
+	} else {
+		submitBtn.setAttribute('disabled', 'true')
+		submitBtn.style.color = "rgba(255, 255, 255, .5)"
 	}
+}
 
-	submitBtn.setAttribute('disabled','true');
-	submitBtn.style.color = "rgba(255, 255, 255, .5)"
+submitBtn.setAttribute('disabled', 'true');
+submitBtn.style.color = "rgba(255, 255, 255, .5)"
 	
+for (input of validateIt) {
+	input.addEventListener('input', function () {
+		const validationResult = validateField(this.getAttribute('name'), this.value);
+		changeColor(this, validationResult);
+		allowSubmit();
+	})
+}
 
-	for (input of validateIt){
-		input.addEventListener('input', function(){
-			if (this.getAttribute('name') === 'email'){
-				colorChanger(this, emailRegExp.test(this.value));
-			}else
-			if (this.getAttribute('name') === 'name' || this.getAttribute('name') === 'surname'){
-				colorChanger(this, nameRegExp.test(this.value))
-			}else
-			if (this.getAttribute('name') === 'phoneNumber'){
-				colorChanger(this, phoneNumberRegExp.test(this.value))
-			}else
-			if (this.getAttribute('name') === 'toReserveDate1'){
-				colorChanger(this, dateRegExp.test(this.value))
-			}}
-	)}
+hourObserver.observe(hour, { attributes: true });
 
-	
-	let hourObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function() {
-			if (hour.getAttribute('value')){
-				validationResult[hour.getAttribute('name')] = true;
-				allowSubmit();
-			}else{
-				validationResult[hour.getAttribute('name')] = false
-				allowSubmit();	
-			}
-		});
-	});
-	hourObserver.observe(hour, {attributes:true});
-	
-	let durationObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function() {
-			if (duration.getAttribute('value')){
-				validationResult[duration.getAttribute('name')] = true;
-				allowSubmit();
-			}else{
-				validationResult[duration.getAttribute('name')] = false
-				allowSubmit();	
-			}
-		});
-	});
-	durationObserver.observe(duration, {attributes:true});
-	
-
-	const colorChanger = (obj, res) => {
-		if (res){
-			obj.style.color = "rgba(255, 255, 255, 1)";
-			validationResult[obj.getAttribute('name')] = true;
-			allowSubmit();
-		}else{
-			obj.style.color = "rgba(255, 255, 255, .5)";
-			validationResult[obj.getAttribute('name')] = false;
-			allowSubmit();
-		}
-	}
-	
-	const allowSubmit = () => {
-		!validationResult.isAllTrue() 
-			? submitBtn.setAttribute('disabled','true')
-			: submitBtn.removeAttribute('disabled') 
-				? submitBtn.style.color = "rgba(255, 255, 255, .5)"
-				: submitBtn.style.color = "rgba(255, 255, 255, 1)"
-	}
+durationObserver.observe(duration, { attributes: true });
